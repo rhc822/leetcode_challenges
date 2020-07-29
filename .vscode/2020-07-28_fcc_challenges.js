0143fafb2f779657103526e1776ec02c8e611b33
rhc822 <rhc822@gmail.com>
2020-07-28 20:13:50 -0500

    Assigned group with two others and worked together on question 2: Given a positive number less than 100,000, calculate the square root without using library or built in methods.

---
+++ .vscode/2020-07-28_fcc_challenges.js
@@ -0,0 +1,31 @@
+// https://www.freecodecamp.org/news/find-square-root-of-number-calculate-by-hand/
+
+
+// https://docs.google.com/document/d/1RjqPdF3Uie78ymWlFhTxpdqotbkS_oeZ865w-3ehPh4/edit
+
+// function something (given a # < 100,000){
+
+//   a number do a calculation to that
+//   return value
+
+// }
+
+const returnSqrt = (number) => {
+
+    //   a number do a calculation to that
+    //   return value
+    // split number into pairs
+      let n = number.toString().length
+      if (n % 2 !== 0) {
+        newN = n + "0"
+      } else {
+
+      }
+
+      return n;
+
+    }
+
+    const answer = returnSqrt(2025);
+
+    console.log(answer);
