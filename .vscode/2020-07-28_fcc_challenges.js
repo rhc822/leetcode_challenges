809f2f03baf7ceb60bafd7cbff4054624e5bbfb4
rhc822 <rhc822@gmail.com>
2020-07-31 10:08:43 -0500

    Wrote out plan in comments and started the first one, 'calculating all perfect squares under 100,000 and store values in an array'. Also, trying to get run and debug working for this in VSCode

--- .vscode/2020-07-28_fcc_challenges.js
+++ .vscode/2020-07-28_fcc_challenges.js
@@ -1,3 +1,69 @@
+/*
+
+Given a positive number less than 100,000, calculate the square root without using library or built in methods.
+
+example:
+findSquareRoot(9) // 3
+findSquareRoot(15) // 3.872  or something close to it
+findSquareRoot(576) // 24
+findSquareRoot(63504) // 252
+
+*/
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// 1) calculate all perfect squares under 100,000 and store values in an array
+
+const perfectSquares = [1, 2, 3];
+console.log(perfectSquares)
+perfectSquares += 1;
+console.log(perfectSquares)
+
+// for (i = 1; i < 100,000; i++){
+
+//     if (i * i === 5) {
+
+//         perfectSquares += i;
+
+//     }
+
+// }
+
+// console.log(perfectSquares);
+
+// 2) Take the input of a given number and check if it is > 1
+
+
+
+// 3) Find the nearest perfect squares on either side of given number
+
+
+
+// 4) Divde the given number by the lower perfect square number
+
+
+
+// 5) Average the resulting answer and the lower perfect square number together
+
+
+
+// 6) Take that resulting answer Test if answer is to desired accuracy
+
+
+
+// 7) If so, display answer. If not, repeat
+
+
+
+
+
+
+
+
+
+
+
+
 // https://www.freecodecamp.org/news/find-square-root-of-number-calculate-by-hand/
 
 
@@ -10,22 +76,28 @@
 
 // }
 
-const returnSqrt = (number) => {
+// ---------------------------------
 
-    //   a number do a calculation to that
-    //   return value
-    // split number into pairs
-      let n = number.toString().length
-      if (n % 2 !== 0) {
-        newN = n + "0"
-      } else {
+// What my team and I started last time...
 
-      }
+// const returnSqrt = (number) => {
 
-      return n;
+//     //   a number do a calculation to that
+//     //   return value
+//     // split number into pairs
 
-    }
 
-    const answer = returnSqrt(2025);
+//     let n = number.toString().length
+//       if (n % 2 !== 0) {
+//         newN = n + "0"
+//       } else {
 
-    console.log(answer);
+//       }
+
+//       return n;
+
+//     }
+
+//     const answer = returnSqrt(2025);
+
+//     console.log(answer);
